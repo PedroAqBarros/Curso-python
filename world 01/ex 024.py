231f0a9b288fa1b99e1d5741246787617ff11fd0
Pedro Aquino Barros <pkzinfps@gmail.com>
2025-02-14 19:15:02 -0300

    dia 03

--- world 01/ex 024.py
+++ world 01/ex 024.py
@@ -1,4 +1,4 @@
-nome = (input('Digite o nome de uma cidade: ')).strip().lower().split()
+nome = input('Digite o nome de uma cidade: ').strip().lower().split()
 if nome[0] == 'santo':
     print('verdadeiro')
 else :
