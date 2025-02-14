08d278eea19335bbad257174538ddabe587d3c2c
Pedro Aquino Barros <pkzinfps@gmail.com>
2025-02-14 16:51:06 -0300

    0

--- world 01/ex 019.py
+++ world 01/ex 019.py
@@ -4,4 +4,4 @@ a2 = str(input('Digite o nome do segundo aluno: '))
 a3 = str(input('Digite o anome do terceiro aluno: '))
 a4 = str(input('Digite o nome do quarto aluno: '))
 sorteio = [a1, a2, a3, a4] 
-print(random.choice(sorteio))
+print('O aluno sorteado é:',random.choice(sorteio))
