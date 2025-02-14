2bc4415980547e84310aa5aa0ee5e3e2b5930fd4
PedroAqBarros <pkzinfps@gmail.com>
2025-02-14 18:35:08 -0300

    0

--- world 01/ex 023.py
+++ world 01/ex 023.py
@@ -1,1 +1,3 @@
-num = int(input('Digite um numero de 0 a 9999: '))
+num = str(input('Digite um numero de 4 digitos: '))
+lista = list(num)
+print('Unidade {}, Dezena {}, Centena {}, Milhar {}'.format(lista[3],lista[2],lista[1],lista[0]))
